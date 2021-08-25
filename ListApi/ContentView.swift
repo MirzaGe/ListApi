672b85dac5fd981fd44f59d046503ce0595e2e3b
Sherry1481 <shareyscorpi1@gmail.com>
2021-08-26 03:08:10 +0500

    Initial Commit

---
+++ ListApi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListApi
+//
+//  Created by sherry on 26/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ListApi/ListApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListApiApp.swift
+//  ListApi
+//
+//  Created by sherry on 26/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct ListApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
